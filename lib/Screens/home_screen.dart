8528881a21d006793a1ca5bf86f1be07a4cb8c91
NareshJaipal <132568227+NareshJaipal@users.noreply.github.com>
2023-07-04 07:58:30 -0700

    Chat App
    My first flutter app

---
+++ lib/Screens/home_screen.dart
@@ -0,0 +1,303 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:get/get.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:chat_room/Controller/controller.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  final Controller _controller = Get.put(Controller());
+  final _messageCollection = FirebaseFirestore.instance.collection('chats');
+  final GlobalKey<FormState> key = GlobalKey<FormState>();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        title: const Text('Public Group Chat',
+            style: TextStyle(fontFamily: "Bassy", fontSize: 30)),
+        flexibleSpace: Container(
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight,
+              colors: [
+                Colors.blue,
+                Colors.lightBlue,
+                Colors.blueGrey,
+              ],
+            ),
+          ),
+        ),
+        systemOverlayStyle: SystemUiOverlayStyle.light,
+      ),
+
+      /// StreamBuilder is used to get the data from the firestore
+      /// QuerySnapshot is a snapshot of the data from the firestore
+      body: StreamBuilder<QuerySnapshot>(
+        /// stream is used to get the data from the firestore via the collection
+        stream: _messageCollection.snapshots(),
+        // builder is used to build the listview/view
+        builder: (context, snapshot) {
+          if (!snapshot.hasData) {
+            return const Center(
+              child: CircularProgressIndicator(),
+            );
+          }
+          final memoDocs = snapshot.data!.docs;
+          return ListView.builder(
+            physics: const BouncingScrollPhysics(),
+            itemCount: memoDocs.length,
+            itemBuilder: (context, index) {
+              final memoDoc = memoDocs[index];
+
+              return Dismissible(
+                key: Key(memoDoc.id),
+                onDismissed: (direction) =>
+                    _messageCollection.doc(memoDoc.id).delete(),
+                child: Padding(
+                  padding: const EdgeInsets.fromLTRB(5, 5, 5, 0),
+                  child: Card(
+                    color: Colors.blueAccent[50],
+                    elevation: 4,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                    clipBehavior: Clip.antiAlias,
+                    child: ListTile(
+                      leading: SizedBox(
+                        height: 30,
+                        width: 30,
+                        child: Container(
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(50),
+                            gradient: const LinearGradient(
+                              begin: Alignment.topLeft,
+                              end: Alignment.bottomRight,
+                              colors: [
+                                Colors.blue,
+                                Colors.blueGrey,
+                                Colors.lightBlue,
+                              ],
+                            ),
+                          ),
+                          child: Center(
+                            child: Text(
+                              index.toString(),
+                              style: GoogleFonts.handlee(
+                                color: Colors.white,
+                                fontSize: 15,
+                              ),
+                            ),
+                          ),
+                        ),
+                      ),
+                      title: Padding(
+                        padding: const EdgeInsets.symmetric(vertical: 5),
+                        child: Text(
+                          memoDoc['message'],
+                          style: GoogleFonts.handlee(),
+                        ),
+                      ),
+                      subtitle: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Row(
+                            children: [
+                              const Icon(Icons.date_range_rounded,
+                                  size: 15, color: Colors.grey),
+                              const SizedBox(width: 5),
+                              Text(
+                                memoDoc['date'],
+                                style: const TextStyle(
+                                  fontSize: 12,
+                                ),
+                              ),
+                            ],
+                          ),
+                          Row(
+                            children: [
+                              const Icon(Icons.insert_drive_file,
+                                  size: 15, color: Colors.grey),
+                              const SizedBox(width: 5),
+                              Text(
+                                memoDoc.id,
+                                style: const TextStyle(
+                                  fontSize: 10,
+                                  color: Colors.grey,
+                                ),
+                              ),
+                            ],
+                          ),
+                        ],
+                      ),
+                      onLongPress: () {
+                        showDialog(
+                          context: context,
+                          builder: (BuildContext context) {
+                            return AlertDialog(
+                              content: Container(
+                                padding: const EdgeInsets.all(1),
+                                height: 85,
+                                width: 150,
+                                child: Center(
+                                  child: Column(
+                                    children: [
+                                      GestureDetector(
+                                        child: const Row(
+                                          children: [
+                                            Icon(
+                                              Icons.edit,
+                                              size: 30,
+                                            ),
+                                            SizedBox(width: 10),
+                                            Text("Edit"),
+                                          ],
+                                        ),
+                                        onTap: () {
+                                          showDialog(
+                                            context: context,
+                                            builder: (BuildContext context) {
+                                              return AlertDialog(
+                                                content: Form(
+                                                  key: key,
+                                                  child: TextFormField(
+                                                    style:
+                                                        GoogleFonts.handlee(),
+                                                    maxLines: 1,
+                                                    initialValue:
+                                                        memoDoc['message'],
+                                                    onChanged: (value) {
+                                                      _messageCollection
+                                                          .doc(memoDoc.id)
+                                                          .update({
+                                                        'message': value
+                                                      });
+                                                    },
+                                                    validator: (value) {
+                                                      if (value!.isEmpty) {
+                                                        return 'Please enter some text';
+                                                      }
+                                                      return null;
+                                                    },
+                                                  ),
+                                                ),
+                                              );
+                                            },
+                                          );
+                                        },
+                                      ),
+                                      const SizedBox(height: 20),
+                                      GestureDetector(
+                                        child: const Row(
+                                          children: [
+                                            Icon(
+                                              Icons.delete_forever,
+                                              size: 30,
+                                              color: Colors.red,
+                                            ),
+                                            SizedBox(width: 10),
+                                            Text("Delete")
+                                          ],
+                                        ),
+                                        onTap: () {
+                                          _messageCollection
+                                              .doc(memoDoc.id)
+                                              .delete();
+                                        },
+                                      ),
+                                    ],
+                                  ),
+                                ),
+                              ),
+                            );
+                          },
+                        );
+                      },
+                    ),
+                  ),
+                ),
+              );
+            },
+          );
+        },
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+      floatingActionButton: FloatingActionButton(
+        backgroundColor: Colors.blue,
+        child: const Icon(Icons.chat),
+        onPressed: () {
+          showDialog(
+            context: context,
+            builder: (BuildContext context) {
+              return AlertDialog(
+                content: Form(
+                  key: key,
+                  child: Column(
+                    mainAxisSize: MainAxisSize.min,
+                    children: <Widget>[
+                      TextFormField(
+                        style: GoogleFonts.handlee(),
+                        maxLines: 1,
+                        decoration: InputDecoration(
+                          labelText: 'Message',
+                          labelStyle: GoogleFonts.handlee(),
+                        ),
+                        validator: (val) {
+                          if (val!.isEmpty) {
+                            return '*Required';
+                          } else {
+                            return null;
+                          }
+                        },
+                        onChanged: (val) {
+                          _controller.message.value = val;
+                        },
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.only(top: 8.0),
+                        child: ElevatedButton(
+                          child: const Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            crossAxisAlignment: CrossAxisAlignment.center,
+                            children: [
+                              Text("Send"),
+                              SizedBox(width: 5),
+                              Icon(Icons.send_rounded, size: 15),
+                            ],
+                          ),
+                          onPressed: () {
+                            if (key.currentState!.validate()) {
+                              key.currentState!.save();
+                              Get.back();
+
+                              _messageCollection.add(
+                                {
+                                  'message': _controller.message.value,
+                                  'date':
+                                      '${DateTime.now().day}.${DateTime.now().month}.${DateTime.now().year}',
+                                },
+                              );
+                            }
+                          },
+                        ),
+                      )
+                    ],
+                  ),
+                ),
+              );
+            },
+          );
+        },
+      ),
+    );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,27 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'Screens/home_screen.dart';
+import 'firebase_options.dart';
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  runApp(const Home());
+}
+
+class Home extends StatelessWidget {
+  const Home({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return GetMaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Public Group Chat',
+      theme: ThemeData(primarySwatch: Colors.blue),
+      home: const HomeScreen(),
+    );
+  }
+}
